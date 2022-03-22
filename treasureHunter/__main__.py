2e0d5dc6b17d8d45dd5e87dede63671199d5b02f
Quibly <trippy524@yahoo.com>
2022-03-22 11:14:36 -0600

    created constants file

--- treasureHunter/__main__.py
+++ treasureHunter/__main__.py
@@ -1,4 +1,4 @@
-import os
+import constants
 import random
 
 from game.casting.actor import Actor
@@ -7,7 +7,6 @@ from game.casting.treasure import Treasure
 from game.casting.trap import Trap
 from game.casting.hunter import Hunter
 from game.casting.cast import Cast
-
 from game.directing.director import Director
 
 from game.services.keyboard_service import KeyboardService
@@ -15,27 +14,6 @@ from game.services.video_service import VideoService
 
 from game.shared.color import Color
 from game.shared.point import Point
-
-
-FRAME_RATE = 12
-MAX_X = 900
-MAX_Y = 600
-CELL_SIZE = 15
-FONT_SIZE = 15
-COLS = 60
-ROWS = 40
-CAPTION = "Treasure Hunter"
-DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/data/messages.txt"
-WHITE = Color(255, 255, 255)
-POWDER_BLUE = Color(240 ,248, 255)
-BLACK = Color(0, 0, 0)
-RED = Color(255, 0, 0)
-TAN = Color(160, 82, 45)
-TREASURE_VALUE = 1
-TRAP_DAMAGE = 50
-DEFAULT_TREASURES = 6
-DEFAULT_TRAPS = 4
-HUNTERS = 1
 
 
 def main():
@@ -49,85 +27,85 @@ def main():
     # create the banner
     banner = Actor()
     banner.set_text("")
-    banner.set_font_size(FONT_SIZE)
-    banner.set_color(WHITE)
-    banner.set_position(Point(CELL_SIZE, 0))
+    banner.set_font_size(constants.FONT_SIZE)
+    banner.set_color(constants.WHITE)
+    banner.set_position(Point(constants.CELL_SIZE, 0))
     cast.add_actor("banners", banner)
     
     # create the hunters
-    x = int(MAX_X / 4)
-    y = int(MAX_Y - CELL_SIZE)
+    x = int(constants.MAX_X / 4)
+    y = int(constants.MAX_Y - constants.CELL_SIZE)
     position = Point(x, y)
 
-    for i in range(HUNTERS):
+    for i in range(constants.HUNTERS):
         if i == 2:
             position = Point(x*3,y)
         hunter = Hunter()
         hunter.set_text("#")
-        hunter.set_font_size(FONT_SIZE)
-        hunter.set_color(POWDER_BLUE)
+        hunter.set_font_size(constants.FONT_SIZE)
+        hunter.set_color(constants.POWDER_BLUE)
         hunter.set_position(position)
         cast.add_actor("hunters", hunter)
 
     
     # create the ground cover
-    for i in range(COLS):
-        for j in range(ROWS):
+    for i in range(constants.COLS):
+        for j in range(constants.ROWS):
             text = chr(random.randint(33, 126))
             position = Point(i,j)
-            position = position.scale(CELL_SIZE)
+            position = position.scale(constants.CELL_SIZE)
             
             ground_cover = Ground_Cover()
             ground_cover.set_text(text)
-            ground_cover.set_font_size(FONT_SIZE)
-            ground_cover.set_color(TAN)
+            ground_cover.set_font_size(constants.FONT_SIZE)
+            ground_cover.set_color(constants.TAN)
             ground_cover.set_position(position)
             cast.add_actor("ground_covers", ground_cover)
 
     # create the treasures
-    with open(DATA_PATH) as file:
+    with open(constants.DATA_PATH) as file:
         data = file.read()
         messages = data.splitlines()
 
-    for n in range(DEFAULT_TREASURES):
+    for n in range(constants.DEFAULT_TREASURES):
         text = "X"
         message = messages[n]
 
-        x = random.randint(1, COLS - 1)
-        y = random.randint(1, ROWS - 1)
+        x = random.randint(1, constants.COLS - 1)
+        y = random.randint(1, constants.ROWS - 1)
         position = Point(x, y)
-        position = position.scale(CELL_SIZE)
+        position = position.scale(constants.CELL_SIZE)
         
         treasure = Treasure()
         treasure.set_text(text)
-        treasure.set_font_size(FONT_SIZE)
-        treasure.set_color(BLACK)
+        treasure.set_font_size(constants.FONT_SIZE)
+        treasure.set_color(constants.BLACK)
         treasure.set_position(position)
         treasure.set_message(message)
-        treasure.set_value(TREASURE_VALUE)
+        treasure.set_value(constants.TREASURE_VALUE)
         cast.add_actor("treasures", treasure)
 
     # create the traps
-    for n in range(DEFAULT_TRAPS):
+    for n in range(constants.DEFAULT_TRAPS):
         text = "O"
 
-        x = random.randint(1, COLS - 1)
-        y = random.randint(1, ROWS - 1)
+        x = random.randint(1, constants.COLS - 1)
+        y = random.randint(1, constants.ROWS - 1)
         position = Point(x, y)
-        position = position.scale(CELL_SIZE)
+        position = position.scale(constants.CELL_SIZE)
 
         trap = Trap()
         trap.set_text(text)
-        trap.set_font_size(FONT_SIZE)
-        trap.set_color(BLACK)
+        trap.set_font_size(constants.FONT_SIZE)
+        trap.set_color(constants.BLACK)
         trap.set_position(position)
-        trap.set_damage(TRAP_DAMAGE)
+        trap.set_damage(constants.TRAP_DAMAGE)
         cast.add_actor("traps", trap)
                 
     
     # start the game
-    keyboard_service = KeyboardService(CELL_SIZE)
-    video_service = VideoService(CAPTION, MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE)
+    keyboard_service = KeyboardService(constants.CELL_SIZE)
+    video_service = VideoService(constants.CAPTION, constants.MAX_X, constants.MAX_Y, constants.CELL_SIZE, constants.FRAME_RATE)
     director = Director(keyboard_service, video_service)
     director.start_game(cast)
 
---
+++ treasureHunter/constants.py
@@ -0,0 +1,22 @@
+from game.shared.color import Color
+import os
+
+FRAME_RATE = 12
+MAX_X = 900
+MAX_Y = 600
+CELL_SIZE = 15
+FONT_SIZE = 15
+COLS = 60
+ROWS = 40
+CAPTION = "Treasure Hunter"
+DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/data/messages.txt"
+WHITE = Color(255, 255, 255)
+POWDER_BLUE = Color(240 ,248, 255)
+BLACK = Color(0, 0, 0)
+RED = Color(255, 0, 0)
+TAN = Color(160, 82, 45)
+TREASURE_VALUE = 1
+TRAP_DAMAGE = 50
+DEFAULT_TREASURES = 6
+DEFAULT_TRAPS = 4
+HUNTERS = 1
