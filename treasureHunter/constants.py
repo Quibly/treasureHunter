c920af25923fc1bfaa33cf2dc80929f92dbb9114
Quibly <92229762+Quibly@users.noreply.github.com>
2022-03-28 16:35:12 -0600

    multi-player, scoring, health, message, game over

--- treasureHunter/constants.py
+++ treasureHunter/constants.py
@@ -2,13 +2,13 @@ from game.shared.color import Color
 import os
 
 FRAME_RATE = 12
-MAX_X = 900
-MAX_Y = 600
-BANNER_HEIGHT = 2
+BANNER_HEIGHT = 4
 CELL_SIZE = 15
 FONT_SIZE = 15
 COLS = 60
 ROWS = 40
+MAX_X = 900
+MAX_Y = 600 + (BANNER_HEIGHT * CELL_SIZE)
 CAPTION = "Treasure Hunter"
 DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/data/messages.txt"
 WHITE = Color(255, 255, 255)
@@ -20,4 +20,5 @@ TREASURE_VALUE = 1
 TRAP_DAMAGE = 50
 DEFAULT_TREASURES = 6
 DEFAULT_TRAPS = 4
-HUNTERS = 1
+HUNTERS = 2
+HUNTER_HEALTH = 100
