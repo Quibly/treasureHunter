48500907afe39a7a2a3b55c9a5d55ea959dd91bf
Quibly <92229762+Quibly@users.noreply.github.com>
2022-03-28 16:39:42 -0600

    multi-player, scoring, health, message, game over

--- treasureHunter/game/casting/hunter.py
+++ treasureHunter/game/casting/hunter.py
@@ -1,6 +1,7 @@
 from game.casting.actor import Actor
-import constants
 from game.shared.point import Point
+import constants
+from constants import HUNTER_HEALTH
 
 class Hunter(Actor):
     """
@@ -11,13 +12,15 @@ class Hunter(Actor):
     Attributes:
         _damage(int): The amount of damage a player takes if they contact this trap.
     """
-    def __init__(self):
+    def __init__(self, player_number):
         """
         Holds the damage amount from the trap
         """
         super().__init__()
         self._health = 100
-        self._prepare_hunter()
+        self._points = 0
+        self._player_number = 1
+        self._prepare_hunter(player_number)
 
     def get_health(self):
         """Returns the damage amount of the trap.
@@ -26,25 +29,56 @@ class Hunter(Actor):
             _damage(int): The amount of damage a player takes if they contact this trap.
         """
         return self._health
+
+    def set_health(self, health):
+        """
+        """
+        self._health = health
     
-    def change_health(self, change):
+    def trap_damage(self):
         """Updates the damage amount from contacting the trap.
         
         Args:
             _damage(int): The amount of damage a player takes if they contact this trap.
         """
-        health = self._health
-        self._health = int(change) + health
+        self._health = self._health - 50
+        
+    def get_points(self):
+        """
+        """
+        return self._points
 
-    def _prepare_hunter(self):
+    def set_points(self, points=0):
         """
         """
-        x = int(constants.MAX_X / 4)
-        y = int(constants.MAX_Y - constants.CELL_SIZE)
-        position = Point(x, y)
+        self._points = self._points + points
+
+    def get_player_number(self):
+        """
+        """
+        return self._player_number
+    
+    def set_player_number(self, player_number):
+        """
+        """
+        self._player_number = player_number
+
+    def _prepare_hunter(self, player_number):
+        """
+        """
+        if player_number == 1:
+            x = int(constants.MAX_X / 4)
+            y = int(constants.MAX_Y - constants.CELL_SIZE)
+            position = Point(x, y)
+        elif player_number == 2:
+            x = int((constants.MAX_X * 3) / 4)
+            y = int(constants.MAX_Y - constants.CELL_SIZE)
+            position = Point(x, y)
         text = "#"
         self.set_text(text)
         self.set_font_size(constants.FONT_SIZE)
         self.set_color(constants.POWDER_BLUE)
         self.set_position(position)
-
+        self.set_health(HUNTER_HEALTH)
+        self.set_points()
+        self.set_player_number(player_number)
